07da0855af1542634c4f5e24191a4f19288b5909
pratikxanand <pratikxanand@gmail.com>
2026-07-21 00:28:33 +0530

    Updated website

--- web.js
+++ web.js
@@ -1,9 +1,50 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
-    .then(() => {
-      console.log("Service Worker Registered");
+const CACHE_NAME = "my-app-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+
+  // CSS
+  "./css/style.css",
+
+  // JS
+  "./js/script.js",
+  "./js/pwa.js",
+
+  // Images
+  "./assets/logo-192.png",
+  "./assets/logo-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
-    .catch((error) => {
-      console.log("Service Worker Error:", error);
-    });
-}
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
