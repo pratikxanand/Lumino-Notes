bc5162ce0f2d4c57185b0ac3d11e76c01b6ee9da
pratikxanand <pratikxanand@gmail.com>
2026-07-21 00:27:01 +0530

    Updated website

---
+++ js/service-worker.js
@@ -0,0 +1,20 @@
+const cacheName = "Lumino Notes-v1";
+
+const assets = [
+  "/",
+  "/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
